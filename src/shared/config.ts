729e5cd7deca24a188cc11a829b5d8d1522394dd
Кравцов Роман 8К24 <rdk5@tpu.ru>
2025-03-30 01:42:30 +0700

    updates from max

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,8 +1,2 @@
-import axios from "axios";
-
 const testUrl: string = "http://82.202.138.236:31064"
 export const API_URL: string = testUrl + "/auth/api/v1";
-
-export const apiAxios = axios.create({
-    baseURL: API_URL,
-})
